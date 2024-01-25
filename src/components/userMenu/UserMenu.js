c94cc59c0ee5bea8afbd89fd0be0706dfbac6890
g1Den1s-it <g1den1splay@gmail.com>
2024-01-25 13:32:54 +0200

    added user menu

--- src/components/userMenu/UserMenu.js
+++ src/components/userMenu/UserMenu.js
@@ -1,22 +1,43 @@
 import {useState} from "react";
 import "./UserMenu.css";
+
 const UserMenu = () => {
-    let isAuth = false;
-    const [user, isUser] = useState({user: {image: '', username: ''}})
-    if(!isAuth){
-        return(
-            <div className="login">
-                <a className="login-button" href="/sign-in/">sign in</a>
-                <a className="login-button" href="/sign-up/">sign up</a>
-            </div>
-        );
-    }else {
-        return(
+    const [isAuth, setIsAuth] = useState(false);
+    const [user, isUser] = useState({user: {image: '', username: ''}});
+
+    const activeHandler = (e) => {
+        const userMenu = document.querySelector(".login");
+        const body = document.querySelector("body");
+
+        e.target.classList.toggle("active");
+        userMenu.classList.toggle("active");
+        body.classList.toggle("lock");
+    }
+
+    return(
+    <div className="user-menu">
+        <div className="user-menu-burger" onClick={activeHandler}>
+            <span></span>
+        </div>
+        {isAuth
+            ?
             <div>
-                <img src={user.image} alt="user image"/>
+                <img src={user.image} alt="user"/>
                 <p>{user.username}</p>
             </div>
-        );
-    }
+            :
+            <div className="login">
+                <ul>
+                    <li>
+                        <a className="login-button" href="/sign-in/">sign in</a>
+                    </li>
+                    <li>
+                        <a className="login-button" href="/sign-up/">sign up</a>
+                    </li>
+                </ul>
+            </div>
+        }
+    </div>
+    );
 }
 export default UserMenu;
