1d405199c0878fe1107f2e7f60505737eb52d4dc
g1Den1s-it <g1den1splay@gmail.com>
2024-02-11 20:49:15 +0200

    updated design sign up page

--- src/pages/signUp/signUp.js
+++ src/pages/signUp/signUp.js
@@ -1,12 +1,24 @@
 import "./signUp.css"
 import {SignUpForm} from "../../modules";
+import {Logo} from "../../ui";
 
 const SignUp = () =>{
+    const loginImage = "/login/login.png";
+
     return(
         <div className="sign-up">
-            <div className="sign-up__container">
-                <SignUpForm />
-                <div className="decoration"></div>
+
+            <div className="sign-up__holder">
+                <div className="sign-up__holder_wrapper">
+                    <Logo url="/">Mindtify</Logo>
+                    <img className="sign-up__img" src={loginImage}/>
+                </div>
+            </div>
+            <div className="sign-up__wrapper">
+                <div className="sign-up__logo">
+                    <Logo url="/">Mindtify</Logo>
+                </div>
+                <SignUpForm/>
             </div>
         </div>
     );
