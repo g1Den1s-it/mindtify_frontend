3977face30da90473a5bbf30151a91896ab276a8
g1Den1s-it <g1den1splay@gmail.com>
2024-03-10 15:48:33 +0200

    added links

--- src/pages/signUp/signUp.js
+++ src/pages/signUp/signUp.js
@@ -1,13 +1,13 @@
 import "./signUp.css"
 import {SignUpForm} from "../../modules";
 import {Logo} from "../../ui";
+import {Link, useLocation} from "react-router-dom";
 
 const SignUp = () =>{
     const loginImage = "/login/login.png";
-
+    const location = useLocation();
     return(
         <div className="sign-up">
-
             <div className="sign-up__holder">
                 <div className="sign-up__holder_wrapper">
                     <Logo url="/">Mindtify</Logo>
@@ -17,6 +17,10 @@ const SignUp = () =>{
             <div className="sign-up__wrapper">
                 <div className="sign-up__logo">
                     <Logo url="/">Mindtify</Logo>
+                </div>
+                <div className="page-link">
+                    <Link className={`page-link_a ${location.pathname === "/sign-up/" ? "current-page": ""}`} to="/sign-up/">Sign up</Link>
+                    <Link className={`page-link_a ${location.pathname === "/login/" ? "current-page": ""}`} to="/login/">Sign in</Link>
                 </div>
                 <SignUpForm/>
             </div>
